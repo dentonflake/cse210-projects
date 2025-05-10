f82c2eb3342449fba70e2c81a1fa36f4e21ab1bc
Denton Flake <me@dentonflake.com>
2025-05-10 11:55:17 -0700

    Completed resume project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job {
+            _jobTitle = "Application Developer",
+            _company = "Nellis Auction",
+            _startYear = 2023,
+            _endYear = 2025
+        };
+
+        Job job2 = new Job {
+            _jobTitle = "Construction Worker",
+            _company = "Orange Street Mechanical",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        Resume myResume = new Resume {
+            _name = "Denton Flake",
+            _jobs = {
+                job1,
+                job2
+            }
+        };
+
+        myResume.Display();
     }
 }
