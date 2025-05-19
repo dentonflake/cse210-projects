e34896d2ff537e337ce9c03830756c32f67fccad
Denton Flake <me@dentonflake.com>
2025-05-19 10:17:16 -0700

    Added fraction class

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -4,40 +4,12 @@ public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
 
-    public void Menu()
-    {
-        Console.WriteLine("Welcome to the Journal Program!");
-        Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-
-        Console.Write("What would you like to do? ");
-
-        string response =  Console.ReadLine();
-
-        if (response == "1")
-            AddEntry();
-        else if (response == "2")
-            Display();
-        else if (response == "3")
-            Load();
-        else if (response == "4")
-            Save();
-        else
-            Environment.Exit(0);
-    }
-
     public void AddEntry()
     {
         Entry entry = new Entry();
         entry.New();
 
         _entries.Add(entry);
-
-        Menu();
     }
 
     public void Display()
@@ -46,8 +18,6 @@ public class Journal
         {
             entry.Display();
         }
-
-        Menu();
     }
 
     public void Load()
@@ -69,8 +39,6 @@ public class Journal
 
             _entries.Add(entry);
         }
-
-        Menu();
     }
 
     public void Save()
@@ -85,7 +53,5 @@ public class Journal
                 outputFile.WriteLine($"{entry._date},{entry._prompt},{entry._response}");
             }
         }
-
-        Menu();
     }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,6 +6,31 @@ class Program
     {
         Journal journal = new Journal();
 
-        journal.Menu();
+        do
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("What would you like to do? ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+                journal.AddEntry();
+            else if (choice == "2")
+                journal.Display();
+            else if (choice == "3")
+                journal.Load();
+            else if (choice == "4")
+                journal.Save();
+            else
+                break;
+
+        } while (true);
     }
 }
